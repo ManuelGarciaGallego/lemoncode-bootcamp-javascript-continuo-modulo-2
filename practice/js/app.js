43d1c105841810e4ae9814e8f4227739bcfa9690
Manuel García <manuelgalleggo@gmail.com>
2023-02-22 14:33:42 +0100

    app.js

---
+++ practice/js/app.js
@@ -0,0 +1,5 @@
+var nameHotel = prompt('Introduce el nombre del hotel');
+document.getElementById('hotel-name').innerHTML = 'Hotel ' + nameHotel;
+
+var locationHotel = prompt('Introduce la ubicación del hotel');
+document.getElementById('hotel-location').innerHTML = locationHotel;
