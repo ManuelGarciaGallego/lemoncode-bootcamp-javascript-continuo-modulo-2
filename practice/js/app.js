85c67109f8e712b7ae6e96c118e872f1706575f7
Manuel García <manuelgalleggo@gmail.com>
2023-02-23 11:15:47 +0100

    hotel object

--- practice/js/app.js
+++ practice/js/app.js
@@ -1,5 +1,16 @@
-var nameHotel = prompt('Introduce el nombre del hotel');
-document.getElementById('hotel-name').innerHTML = 'Hotel ' + nameHotel;
+// var nameHotel = prompt('Introduce el nombre del hotel');
+// document.getElementById('hotel-name').innerHTML = 'Hotel ' + nameHotel;
 
-var locationHotel = prompt('Introduce la ubicación del hotel');
-document.getElementById('hotel-location').innerHTML = locationHotel;
+// var locationHotel = prompt('Introduce la ubicación del hotel');
+// document.getElementById('hotel-location').innerHTML = locationHotel;
+
+var hotel = {
+  name: '',
+  location: '',
+};
+
+hotel.name = prompt('Introduce el nombre del hotel');
+document.getElementById('hotel-name').innerHTML = 'Hotel ' + hotel.name
+
+hotel.location = prompt('Introduce la ubicación del hotel');
+document.getElementById('hotel-location').innerHTML = hotel.location
